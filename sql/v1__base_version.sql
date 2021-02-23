ed226d9b6fd505dd323cd213a7e056aff6aeecb9
romber <romber2001@163.com>
2021-02-23 20:00:35 +0800

    1. v1__base_version.sql: add column middleware_type to table t_meta_middleware_cluster_info

--- sql/v1__base_version.sql
+++ sql/v1__base_version.sql
@@ -53,6 +53,7 @@ CREATE TABLE `t_meta_env_info` (
 CREATE TABLE `t_meta_middleware_cluster_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `cluster_name` varchar(100) DEFAULT NULL COMMENT '中间件集群名称',
+  `middleware_type` tinyint(4) NOT NULL DEFAULT '1' COMMENT '中间件类型: 1-shard, 2-proxy',
   `env_id` int(11) NOT NULL COMMENT '环境: 1-online, 2-rel, 3-sit, 4-uat, 5-dev',
   `del_flag` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除标记: 0-未删除, 1-已删除',
   `create_time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
