39a262b1dd91189bc6349ea51c65ce98f253241f
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-05-12 18:00:26 +0800

    complete healthcheck api

--- api/v1/healthcheck/healthcheck.go
+++ api/v1/healthcheck/healthcheck.go
@@ -1,7 +1,28 @@
 package healthcheck
 
 import (
+	"encoding/json"
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/romberli/das/internal/app/healthcheck"
+	"github.com/romberli/das/pkg/message"
+	msghealth "github.com/romberli/das/pkg/message/healthcheck"
+	"github.com/romberli/das/pkg/resp"
+	"github.com/romberli/go-util/constant"
+	"github.com/romberli/log"
+)
+
+const (
+	hOperationIDJSON = "operation_id"
+	hServerIDJSON    = "server_id"
+	hHostIPJSON      = "host_ip"
+	hPortNumJSON     = "port_num"
+	hStartTimeJSON   = "start_time"
+	hEndTimeJSON     = "end_time"
+	hStepJSON        = "step"
+	hReviewJSON      = "review"
 )
 
 // @Tags healthcheck
@@ -10,7 +31,35 @@ import (
 // @Success 200 {string} string "{"code": 200, "data": []}"
 // @Router /api/v1/healthcheck/result/:id [get]
 func GetResultByOperationID(c *gin.Context) {
-
+	// get data
+	operationIDStr := c.Param(hOperationIDJSON)
+	if operationIDStr == constant.EmptyString {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hOperationIDJSON)
+		return
+	}
+	operationID, err := strconv.Atoi(operationIDStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrTypeConversion, err.Error())
+		return
+	}
+	// init service
+	s := healthcheck.NewServiceWithDefault()
+	// get entities
+	err = s.GetResultByOperationID(operationID)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckGetResultByOperationID)
+		return
+	}
+	// marshal service
+	jsonBytes, err := s.MarshalJSON()
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrMarshalService, err.Error())
+		return
+	}
+	// response
+	jsonStr := string(jsonBytes)
+	log.Debug(message.NewMessage(msghealth.DebugHealthcheckGetResultByOperationID, jsonStr).Error())
+	resp.ResponseOK(c, jsonStr, msghealth.InfoHealthcheckGetResultByOperationID, operationID)
 }
 
 // @Tags healthcheck
@@ -19,7 +68,65 @@ func GetResultByOperationID(c *gin.Context) {
 // @Success 200 {string} string "{"code": 200, "data": "healthcheck started.}"
 // @Router /api/v1/healthcheck/check [post]
 func Check(c *gin.Context) {
-
+	// get data
+	data, err := c.GetRawData()
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrGetRawData, err.Error())
+		return
+	}
+	dataMap := make(map[string]string)
+	err = json.Unmarshal(data, &dataMap)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckCheck, err.Error())
+		return
+	}
+	mysqlServerIDStr, mysqlServerIDExists := dataMap[hServerIDJSON]
+	if !mysqlServerIDExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hServerIDJSON)
+		return
+	}
+	mysqlServerID, err := strconv.Atoi(mysqlServerIDStr)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckCheck, err.Error())
+	}
+	startTimeStr, startTimeExists := dataMap[hStartTimeJSON]
+	if !startTimeExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hStartTimeJSON)
+		return
+	}
+	startTime, err := time.Parse(constant.TimeLayoutSecond, startTimeStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrNotValidTimeLayout, startTimeStr)
+	}
+	endTimeStr, endTimeExists := dataMap[hEndTimeJSON]
+	if !endTimeExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hEndTimeJSON)
+		return
+	}
+	endTime, err := time.Parse(constant.TimeLayoutSecond, endTimeStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrNotValidTimeLayout, endTimeStr)
+	}
+	stepStr, stepExists := dataMap[hStepJSON]
+	if !stepExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hStepJSON)
+		return
+	}
+	step, err := time.ParseDuration(stepStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrNotValidTimeDuration, step)
+	}
+	// init service
+	s := healthcheck.NewServiceWithDefault()
+	// check health
+	err = s.Check(mysqlServerID, startTime, endTime, step)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckCheck)
+		return
+	}
+	respMessage := "healthcheck started"
+	log.Debug(message.NewMessage(msghealth.DebugHealthcheckCheck, respMessage).Error())
+	resp.ResponseOK(c, respMessage, msghealth.InfoHealthcheckCheck)
 }
 
 // @Tags healthcheck
@@ -28,7 +135,71 @@ func Check(c *gin.Context) {
 // @Success 200 {string} string "{"code": 200, "data": ""}"
 // @Router /api/v1/healthcheck/check/host-info [post]
 func CheckByHostInfo(c *gin.Context) {
-
+	// get data
+	data, err := c.GetRawData()
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrGetRawData, err.Error())
+		return
+	}
+	dataMap := make(map[string]string)
+	err = json.Unmarshal(data, &dataMap)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckCheck, err.Error())
+		return
+	}
+	hostIP, hostIPExists := dataMap[hHostIPJSON]
+	if !hostIPExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hHostIPJSON)
+		return
+	}
+	portNumStr, portNumExists := dataMap[hPortNumJSON]
+	if !portNumExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hPortNumJSON)
+		return
+	}
+	portNum, err := strconv.Atoi(portNumStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrTypeConversion, err.Error())
+		return
+	}
+	startTimeStr, startTimeExists := dataMap[hStartTimeJSON]
+	if !startTimeExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hStartTimeJSON)
+		return
+	}
+	startTime, err := time.Parse(constant.TimeLayoutSecond, startTimeStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrNotValidTimeLayout, startTimeStr)
+	}
+	endTimeStr, endTimeExists := dataMap[hEndTimeJSON]
+	if !endTimeExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hEndTimeJSON)
+		return
+	}
+	endTime, err := time.Parse(constant.TimeLayoutSecond, endTimeStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrNotValidTimeLayout, endTimeStr)
+	}
+	stepStr, stepExists := dataMap[hStepJSON]
+	if !stepExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hStepJSON)
+		return
+	}
+	step, err := time.ParseDuration(stepStr)
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrNotValidTimeDuration, step)
+	}
+	// init service
+	s := healthcheck.NewServiceWithDefault()
+	// get entities
+	err = s.CheckByHostInfo(hostIP, portNum, startTime, endTime, step)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckCheckByHostInfo)
+		return
+	}
+	respMessage := "healthcheck by host info started"
+	log.Debug(message.NewMessage(msghealth.DebugHealthcheckCheckByHostInfo, respMessage).Error())
+	resp.ResponseOK(c, respMessage, msghealth.InfoHealthcheckCheckByHostInfo)
 }
 
 // @Tags healthcheck
@@ -37,5 +208,37 @@ func CheckByHostInfo(c *gin.Context) {
 // @Success 200 {string} string "{"code": 200, "data": ""}"
 // @Router /api/v1/healthcheck/review [post]
 func ReviewAccurate(c *gin.Context) {
-
+	// get data
+	data, err := c.GetRawData()
+	if err != nil {
+		resp.ResponseNOK(c, message.ErrGetRawData, err.Error())
+		return
+	}
+	dataMap := make(map[string]int)
+	err = json.Unmarshal(data, &dataMap)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckCheck, err.Error())
+		return
+	}
+	operationID, operationIDExists := dataMap[hOperationIDJSON]
+	if !operationIDExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hOperationIDJSON)
+		return
+	}
+	review, reviewExists := dataMap[hReviewJSON]
+	if !reviewExists {
+		resp.ResponseNOK(c, message.ErrFieldNotExists, hReviewJSON)
+		return
+	}
+	// init service
+	s := healthcheck.NewServiceWithDefault()
+	// review accurate
+	err = s.ReviewAccurate(operationID, review)
+	if err != nil {
+		resp.ResponseNOK(c, msghealth.ErrHealthcheckReviewAccurate)
+		return
+	}
+	respMessage := "reviewed accurate"
+	log.Debug(message.NewMessage(msghealth.DebugHealthcheckReviewAccurate, respMessage).Error())
+	resp.ResponseOK(c, respMessage, msghealth.InfoHealthcheckReviewAccurate)
 }
--- pkg/message/healthcheck/service.go
+++ pkg/message/healthcheck/service.go
@@ -13,22 +13,67 @@ func init() {
 
 const (
 	// debug
-
+	DebugHealthcheckGetResultByOperationID = 101012
+	DebugHealthcheckCheck                  = 101013
+	DebugHealthcheckCheckByHostInfo        = 101014
+	DebugHealthcheckReviewAccurate         = 101015
 	// info
-
+	InfoHealthcheckGetResultByOperationID = 201012
+	InfoHealthcheckCheck                  = 201013
+	InfoHealthcheckCheckByHostInfo        = 201014
+	InfoHealthcheckReviewAccurate         = 201015
 	// error
-	ErrHealthcheckDefaultEngineRun = 401011
+	ErrHealthcheckDefaultEngineRun       = 401011
+	ErrHealthcheckGetResultByOperationID = 401012
+	ErrHealthcheckCheck                  = 401013
+	ErrHealthcheckCheckByHostInfo        = 401014
+	ErrHealthcheckReviewAccurate         = 401015
 )
 
 func initServiceDebugMessage() {
-
+	message.Messages[DebugHealthcheckGetResultByOperationID] = config.NewErrMessage(
+		message.DefaultMessageHeader, DebugHealthcheckGetResultByOperationID,
+		"healthcheck: get result by operation id message: %s")
+	message.Messages[DebugHealthcheckCheck] = config.NewErrMessage(
+		message.DefaultMessageHeader, DebugHealthcheckCheck,
+		"healthcheck: check message: %s")
+	message.Messages[DebugHealthcheckCheckByHostInfo] = config.NewErrMessage(
+		message.DefaultMessageHeader, DebugHealthcheckCheckByHostInfo,
+		"healthcheck: check by host info message: %s")
+	message.Messages[DebugHealthcheckReviewAccurate] = config.NewErrMessage(
+		message.DefaultMessageHeader, DebugHealthcheckReviewAccurate,
+		"healthcheck: review accurate message: %s")
 }
 
 func initServiceInfoMessage() {
+	message.Messages[InfoHealthcheckGetResultByOperationID] = config.NewErrMessage(
+		message.DefaultMessageHeader, InfoHealthcheckGetResultByOperationID,
+		"healthcheck: get result by operation id compeleted. operation_id: %d")
+	message.Messages[InfoHealthcheckCheck] = config.NewErrMessage(
+		message.DefaultMessageHeader, InfoHealthcheckCheck,
+		"healthcheck: check compeleted. %s")
+	message.Messages[InfoHealthcheckCheckByHostInfo] = config.NewErrMessage(
+		message.DefaultMessageHeader, InfoHealthcheckCheckByHostInfo,
+		"healthcheck: check by host info compeleted. %s")
+	message.Messages[InfoHealthcheckReviewAccurate] = config.NewErrMessage(
+		message.DefaultMessageHeader, InfoHealthcheckReviewAccurate,
+		"healthcheck: review accurate compeleted. %s")
 }
 
 func initServiceErrorMessage() {
 	message.Messages[ErrHealthcheckDefaultEngineRun] = config.NewErrMessage(
 		message.DefaultMessageHeader, ErrHealthcheckDefaultEngineRun,
 		"default engine run failed.\n%s")
+	message.Messages[ErrHealthcheckGetResultByOperationID] = config.NewErrMessage(
+		message.DefaultMessageHeader, ErrHealthcheckGetResultByOperationID,
+		"healthcheck: get result by operation id failed. operation_id: %d\n%s")
+	message.Messages[ErrHealthcheckCheck] = config.NewErrMessage(
+		message.DefaultMessageHeader, ErrHealthcheckCheck,
+		"healthcheck: check failed.  %s")
+	message.Messages[ErrHealthcheckCheckByHostInfo] = config.NewErrMessage(
+		message.DefaultMessageHeader, ErrHealthcheckCheckByHostInfo,
+		"healthcheck: check by host info failed. host info %s")
+	message.Messages[ErrHealthcheckReviewAccurate] = config.NewErrMessage(
+		message.DefaultMessageHeader, ErrHealthcheckReviewAccurate,
+		"healthcheck: review accurate failed.  %s")
 }
