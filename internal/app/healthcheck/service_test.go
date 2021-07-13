a1202a06b96b7c60b1e39d43e387818e961a9041
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-07-13 09:34:15 +0800

    remove reduntent parts

--- internal/app/healthcheck/service_test.go
+++ internal/app/healthcheck/service_test.go
@@ -2,7 +2,10 @@ package healthcheck
 
 import (
 	"testing"
+	"time"
 
+	"github.com/jinzhu/now"
+	"github.com/romberli/das/global"
 	"github.com/romberli/go-util/common"
 	"github.com/romberli/go-util/middleware/mysql"
 	"github.com/romberli/log"
@@ -10,79 +13,86 @@ import (
 )
 
 const (
-	serviceDBAddress = "127.0.0.1:3306"
-	serviceHostIP    = "127.0.0.1"
-	servicePortNum   = 3306
-	serviceDBName    = "das"
-	serviceDBUser    = "root"
-	serviceDBPass    = "root"
+	idStruct             = "id"
+	clusterIDStruct      = "ClusterID"
+	serverNameStruct     = "ServerName"
+	hostIPStruct         = "HostIP"
+	portNumStruct        = "PortNum"
+	deploymentTypeStruct = "DeploymentType"
+	versionStruct        = "Version"
 
-	serviceID                           = 1
-	serviceOperationID                  = 1
-	serviceWeightedAverageScore         = 1
-	serviceDBConfigScore                = 1
-	serviceDBConfigData                 = "db config data"
-	serviceDBConfigAdvice               = "db config advice"
-	serviceCPUUsageScore                = 80
-	serviceCPUUsageData                 = "cpu usage data"
-	serviceCPUUsageHigh                 = "cpu usage high"
-	serviceIOUtilScore                  = 80
-	serviceIOUtilData                   = "io util data"
-	serviceIOUtilHigh                   = "io util high"
-	serviceDiskCapacityUsageScore       = 80
-	serviceDiskCapacityUsageData        = "disk capacity usage data"
-	serviceDiskCapacityUsageHigh        = "disk capacity usage high"
-	serviceConnectionUsageScore         = 80
-	serviceConnectionUsageData          = "connection usage data"
-	serviceConnectionUsageHigh          = "connection usage high"
-	serviceAverageActiveSessionNumScore = 80
-	serviceAverageActiveSessionNumData  = "average active session num data"
-	serviceAverageActiveSessionNumHigh  = "average active session num high"
-	serviceCacheMissRatioScore          = 80
-	serviceCacheMissRatioData           = 0.8
-	serviceCacheMissRatioHigh           = 0.8
-	serviceTableSizeScore               = 80
-	serviceTableSizeData                = "table size data"
-	serviceTableSizeHigh                = "table size high"
-	serviceSlowQueryScore               = 80
-	serviceSlowQueryData                = "slow query data"
-	serviceSlowQueryAdvice              = "slow query advice"
-	serviceAccurateReview               = 0
-	serviceDelFlag                      = 0
-
-	serviceMysqlServerID   = 1
-	serviceStartTime       = "2021-05-18 12:00:00.000000"
-	serviceEndTime         = "2021-05-18 15:00:00.000000"
-	serviceStep            = 10
-	servicenewResultStatus = 1
+	defaultMySQLServerInfoClusterID      = 1
+	defaultMySQLServerInfoServerName     = "server1"
+	defaultMySQLServerInfoHostIP         = "127.0.0.1"
+	defaultMySQLServerInfoPortNum        = 3307
+	defaultMySQLServerInfoDeploymentType = 1
+	defaultMySQLServerInfoVersion        = "1.1.1"
 )
 
-func sInitRepository() *Repository {
-	pool, err := mysql.NewPoolWithDefault(serviceDBAddress, serviceDBName, serviceDBUser, serviceDBPass)
-	if err != nil {
-		log.Error(common.CombineMessageWithError("initRepository() failed", err))
-		return nil
-	}
-	return NewRepository(pool)
+func initGlobalMySQLPool() error {
+	dbAddr := dbAddr
+	dbName := dbDBName
+	dbUser := dbDBUser
+	dbPass := dbDBPass
+	maxConnections := mysql.DefaultMaxConnections
+	initConnections := mysql.DefaultInitConnections
+	maxIdleConnections := mysql.DefaultMaxIdleConnections
+	maxIdleTime := mysql.DefaultMaxIdleTime
+	keepAliveInterval := mysql.DefaultKeepAliveInterval
+
+	config := mysql.NewConfig(dbAddr, dbName, dbUser, dbPass)
+	poolConfig := mysql.NewPoolConfigWithConfig(config, maxConnections, initConnections, maxIdleConnections, maxIdleTime, keepAliveInterval)
+	log.Debugf("pool config: %v", poolConfig)
+	var err error
+	global.MySQLPool, err = mysql.NewPoolWithPoolConfig(poolConfig)
+
+	return err
 }
 
-var sRepo = sInitRepository()
-
-func sCreateService() (*Service, error) {
-	var result = NewResult(sRepo, serviceOperationID, serviceWeightedAverageScore, serviceDBConfigScore, serviceDBConfigData, serviceDBConfigAdvice, serviceCPUUsageScore, serviceCPUUsageData, serviceCPUUsageHigh, serviceIOUtilScore, serviceIOUtilData, serviceIOUtilHigh, serviceDiskCapacityUsageScore, serviceDiskCapacityUsageData, serviceDiskCapacityUsageHigh, serviceConnectionUsageScore, serviceConnectionUsageData, serviceConnectionUsageHigh, serviceAverageActiveSessionNumScore, serviceAverageActiveSessionNumData, serviceAverageActiveSessionNumHigh, serviceCacheMissRatioScore, serviceCacheMissRatioData, serviceCacheMissRatioHigh, serviceTableSizeScore, serviceTableSizeData, serviceTableSizeHigh, serviceSlowQueryScore, serviceSlowQueryData, serviceSlowQueryAdvice)
-	err := sRepo.SaveResult(result)
+func createService() (*Service, error) {
+	var result = NewResult(repository,
+		defaultResultOperationID,
+		defaultResultWeightedAverageScore,
+		defaultResultDBConfigScore,
+		defaultResultDBConfigData,
+		defaultResultDBConfigAdvice,
+		defaultResultCPUUsageScore,
+		defaultResultCPUUsageData,
+		defaultResultCPUUsageHigh,
+		defaultResultIOUtilScore,
+		defaultResultIOUtilData,
+		defaultResultIOUtilHigh,
+		defaultResultDiskCapacityUsageScore,
+		defaultResultDiskCapacityUsageData,
+		defaultResultDiskCapacityUsageHigh,
+		defaultResultConnectionUsageScore,
+		defaultResultConnectionUsageData,
+		defaultResultConnectionUsageHigh,
+		defaultResultAverageActiveSessionNumScore,
+		defaultResultAverageActiveSessionNumData,
+		defaultResultAverageActiveSessionNumHigh,
+		defaultResultCacheMissRatioScore,
+		defaultResultCacheMissRatioData,
+		defaultResultCacheMissRatioHigh,
+		defaultResultTableSizeScore,
+		defaultResultTableSizeData,
+		defaultResultTableSizeHigh,
+		defaultResultSlowQueryScore,
+		defaultResultSlowQueryData,
+		defaultResultSlowQueryAdvice)
+	err := repository.SaveResult(result)
 	if err != nil {
 		return nil, err
 	}
 	return &Service{
-		Repository: sRepo,
+		Repository: repository,
 		Result:     result,
 	}, nil
 }
 
-func sDeleteHCResultByOperationID(operationID int) error {
+func deleteHCResultByOperationID(operationID int) error {
 	sql := `delete from t_hc_result where operation_id = ?`
-	_, err := sRepo.Execute(sql, operationID)
+	_, err := repository.Execute(sql, operationID)
 	return err
 }
 
@@ -98,55 +108,69 @@ func TestServiceAll(t *testing.T) {
 func TestService_GetResult(t *testing.T) {
 	asst := assert.New(t)
 
-	service, err := sCreateService()
+	service, err := createService()
 	asst.Nil(err, common.CombineMessageWithError("test GetResult() failed", err))
 	result := service.GetResult()
-	asst.Equal(serviceOperationID, result.GetOperationID(), common.CombineMessageWithError("test GetResult() failed", err))
-	asst.Equal(serviceWeightedAverageScore, result.GetWeightedAverageScore(), common.CombineMessageWithError("test GetResult() failed", err))
+	asst.Equal(defaultResultOperationID, result.GetOperationID(), common.CombineMessageWithError("test GetResult() failed", err))
+	asst.Equal(defaultResultWeightedAverageScore, result.GetWeightedAverageScore(), common.CombineMessageWithError("test GetResult() failed", err))
 	// delete
-	err = sDeleteHCResultByOperationID(serviceOperationID)
+	err = deleteHCResultByOperationID(defaultResultOperationID)
 	asst.Nil(err, common.CombineMessageWithError("test GetResult() failed", err))
 }
 
 func TestService_GetResultByOperationID(t *testing.T) {
 	asst := assert.New(t)
 
-	service, err := sCreateService()
+	service, err := createService()
 	asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
-	err = service.GetResultByOperationID(serviceOperationID)
+	err = service.GetResultByOperationID(defaultResultOperationID)
 	asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
 	result := service.GetResult()
-	asst.Equal(serviceOperationID, result.GetOperationID(), common.CombineMessageWithError("test GetResultByOperationID() failed", err))
-	asst.Equal(serviceWeightedAverageScore, result.GetWeightedAverageScore(), common.CombineMessageWithError("test GetResultByOperationID() failed", err))
+	asst.Equal(defaultResultOperationID, result.GetOperationID(), common.CombineMessageWithError("test GetResultByOperationID() failed", err))
+	asst.Equal(defaultResultWeightedAverageScore, result.GetWeightedAverageScore(), common.CombineMessageWithError("test GetResultByOperationID() failed", err))
 	// delete
-	err = sDeleteHCResultByOperationID(serviceOperationID)
+	err = deleteHCResultByOperationID(defaultResultOperationID)
 	asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
 }
 
 // bug
 func TestService_Check(t *testing.T) {
-	// asst := assert.New(t)
+	asst := assert.New(t)
 
-	// service, err := sCreateService()
+	err := initGlobalMySQLPool()
+	asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
+
+	// mss := metadata.NewMySQLServerServiceWithDefault()
+	// err = mss.Create(map[string]interface{}{
+	// 	idStruct:             defaultResultMysqlServerID,
+	// 	clusterIDStruct:      defaultMySQLServerInfoClusterID,
+	// 	serverNameStruct:     defaultMySQLServerInfoServerName,
+	// 	hostIPStruct:         defaultMySQLServerInfoHostIP,
+	// 	portNumStruct:        defaultMySQLServerInfoPortNum,
+	// 	deploymentTypeStruct: defaultMySQLServerInfoDeploymentType,
+	// 	versionStruct:        defaultMySQLServerInfoVersion})
 	// asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
 
-	// startTime, _ := now.Parse(serviceStartTime)
-	// endTime, _ := now.Parse(serviceEndTime)
-	// step := time.Duration(serviceStep) * time.Second
+	service, err := createService()
+	asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
 
-	// err = service.Check(serviceMysqlServerID, startTime, endTime, step)
-	// asst.Nil(err, common.CombineMessageWithError("test Check(mysqlServerID int, startTime, endTime time.Time, step time.Duration) failed", err))
+	startTime, _ := now.Parse(defaultResultStartTime)
+	endTime, _ := now.Parse(defaultResultEndTime)
+	step := time.Duration(defaultResultStep) * time.Second
 
-	// // delete
-	// err = sDeleteHCResultByOperationID(serviceOperationID)
-	// asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
+	err = service.Check(defaultResultMysqlServerID, startTime, endTime, step)
+	asst.Nil(err, common.CombineMessageWithError("test Check(mysqlServerID int, startTime, endTime time.Time, step time.Duration) failed", err))
+
+	// delete
+	err = deleteHCResultByOperationID(defaultResultOperationID)
+	asst.Nil(err, common.CombineMessageWithError("test GetResultByOperationID() failed", err))
 }
 
 // bug
 func TestService_CheckByHostInfo(t *testing.T) {
 	// asst := assert.New(t)
 
-	// service, err := sCreateService()
+	// service, err := createService()
 	// asst.Nil(err, common.CombineMessageWithError("test CheckByHostInfo(hostIP string, portNum int, startTime, endTime time.Time, step time.Duration) failed", err))
 
 	// startTime, _ := now.Parse(serviceStartTime)
@@ -157,48 +181,48 @@ func TestService_CheckByHostInfo(t *testing.T) {
 	// asst.Nil(err, common.CombineMessageWithError("test CheckByHostInfo(hostIP string, portNum int, startTime, endTime time.Time, step time.Duration) failed", err))
 
 	// // delete
-	// err = sDeleteHCResultByOperationID(serviceOperationID)
+	// err = deleteHCResultByOperationID(serviceOperationID)
 	// asst.Nil(err, common.CombineMessageWithError("test CheckByHostInfo(hostIP string, portNum int, startTime, endTime time.Time, step time.Duration) failed", err))
 }
 
 func TestService_ReviewAccurate(t *testing.T) {
 	asst := assert.New(t)
 
-	service, err := sCreateService()
+	service, err := createService()
 	asst.Nil(err, common.CombineMessageWithError("test ReviewAccurate(id, review int) failed", err))
 	review := 2
-	err = service.ReviewAccurate(serviceOperationID, review)
+	err = service.ReviewAccurate(defaultResultOperationID, review)
 	asst.Nil(err, common.CombineMessageWithError("test ReviewAccurate(id, review int) failed", err))
-	err = service.GetResultByOperationID(serviceOperationID)
+	err = service.GetResultByOperationID(defaultResultOperationID)
 	result := service.GetResult()
 	reviewed := result.GetAccurateReview()
 	asst.Equal(review, reviewed, common.CombineMessageWithError("test ReviewAccurate(id, review int) failed", err))
 	// delete
-	err = sDeleteHCResultByOperationID(serviceOperationID)
+	err = deleteHCResultByOperationID(defaultResultOperationID)
 	asst.Nil(err, common.CombineMessageWithError("test ReviewAccurate(id, review int) failed", err))
 }
 
 func TestService_MarshalJSON(t *testing.T) {
 	asst := assert.New(t)
 
-	service, err := sCreateService()
+	service, err := createService()
 	asst.Nil(err, common.CombineMessageWithError("test MarshalJSON() failed", err))
 	_, err = service.MarshalJSON()
 	asst.Nil(err, common.CombineMessageWithError("test MarshalJSON() failed", err))
 	// delete
-	err = sDeleteHCResultByOperationID(serviceOperationID)
+	err = deleteHCResultByOperationID(defaultResultOperationID)
 	asst.Nil(err, common.CombineMessageWithError("test MarshalJSON() failed", err))
 }
 
 func TestService_MarshalJSONWithFields(t *testing.T) {
 	asst := assert.New(t)
 
-	service, err := sCreateService()
+	service, err := createService()
 	asst.Nil(err, common.CombineMessageWithError("test MarshalJSONWithFields(fields ...string) failed", err))
 	_, err = service.MarshalJSONWithFields("ID", "OperationID", "WeightedAverageScore", "DBConfigScore", "DBConfigData", "DBConfigAdvice", "CPUUsageScore", "CPUUsageData", "CPUUsageHigh", "IOUtilScore", "IOUtilData", "IOUtilHigh", "DiskCapacityUsageScore", "DiskCapacityUsageData", "DiskCapacityUsageHigh", "ConnectionUsageScore", "ConnectionUsageData", "ConnectionUsageHigh", "AverageActiveSessionNumScore", "AverageActiveSessionNumData", "AverageActiveSessionNumHigh", "CacheMissRatioScore", "CacheMissRatioData", "CacheMissRatioHigh", "TableSizeScore", "TableSizeData", "TableSizeHigh", "SlowQueryScore", "SlowQueryData", "SlowQueryAdvice")
 	asst.Nil(err, common.CombineMessageWithError("test MarshalJSONWithFields(fields ...string) failed", err))
 	// delete
-	err = sDeleteHCResultByOperationID(serviceOperationID)
+	err = deleteHCResultByOperationID(defaultResultOperationID)
 	asst.Nil(err, common.CombineMessageWithError("test MarshalJSONWithFields(fields ...string) failed", err))
 }
 
